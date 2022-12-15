6486a6c791987f4fa603c3348e9cb454699147d2
HaolinZhong <hz2771@cumc.columbia.edu>
2022-12-15 03:43:02 -0500

    add google login functionality

--- e6156-frontend/src/actions/userActions.js
+++ e6156-frontend/src/actions/userActions.js
@@ -85,3 +85,34 @@ export const register = (name, email, password) => async (dispatch) => {
         })
     }
 }
+
+export const googleLogin = () => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_LOGIN_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post("/api/users/login/google", config)
+
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type: USER_LOGIN_FAIL,
+            payload:
+                error.response && error.response.data.message ?
+                    error.response.data.message :
+                    error.message
+        })
+    }
+}
--- e6156-frontend/src/screens/LoginScreen.jsx
+++ e6156-frontend/src/screens/LoginScreen.jsx
@@ -1,82 +1,97 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Button, Col, Form, FormControl, FormGroup, FormLabel, Row } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
-import { useLocation, useNavigate } from 'react-router'
+import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import {Button, Col, Form, FormControl, FormGroup, FormLabel, ListGroup, Row} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
+import {useLocation, useNavigate} from 'react-router'
 import FormContainer from '../components/FormContainer'
-import { login } from '../actions/userActions'
+import {login, googleLogin} from '../actions/userActions'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
+import GoogleButton from 'react-google-button'
 
 const LoginScreen = () => {
 
-  let location = useLocation()
-  let navigate = useNavigate()
+    let location = useLocation()
+    let navigate = useNavigate()
 
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-  const userLogin = useSelector((state) => state.userLogin)
-  const { loading, error, userInfo } = userLogin
+    const userLogin = useSelector((state) => state.userLogin)
+    const {loading, error, userInfo} = userLogin
 
-  const redirect = location.search ? location.search.split('=')[1] : ""
+    const redirect = location.search ? location.search.split('=')[1] : ""
 
-  useEffect(() => {
-      if (userInfo) {
-        navigate(redirect)
-      }
-  }, [navigate, userInfo, redirect])
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect)
+        }
+    }, [navigate, userInfo, redirect])
 
-  const submitHandler = (e) => {
-      e.preventDefault()
-      dispatch(login(email, password))
-  }
+    const submitHandler = (e) => {
+        e.preventDefault()
+        dispatch(login(email, password))
+    }
 
-  
+    const googleLoginHandler = (e) => {
+        e.preventDefault()
+        dispatch(googleLogin())
+    }
 
-  return (
-    <FormContainer>
-        <h1 className='my-2'>Sign In</h1>
-        { error && <Message variant='danger'>{error}</Message> }
-        { loading && <Loader /> }
-        <Form onSubmit={submitHandler}>
-            <FormGroup controlId='email' className='py-2'>
-                <FormLabel>Email Address</FormLabel>
-                <FormControl
-                    type='email'
-                    placeholder='Enter email'
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                >
-                </FormControl>
-            </FormGroup>
-            <FormGroup controlId='password' className='py-2'>
-                <FormLabel>Password</FormLabel>
-                <FormControl
-                    type='password'
-                    placeholder='Enter password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                >
-                </FormControl>
-            </FormGroup>
-            <Button type='submit' variant='primary' className='my-3'>
-                Sign In
-            </Button>
-        </Form>
 
-        <Row>
-            <Col>
-                New Customer?{' '}
-                <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
-            </Col>
+    return (
+        <FormContainer className='mx-1'>
+            <h1 className='my-4'>Sign In</h1>
+            {error && <Message variant='danger'>{error}</Message>}
+            {loading && <Loader/>}
+            <Row>
+                <Col md={6}>
+                    <h5 className='my-1'>Sign in with account & password</h5>
+                    <Form onSubmit={submitHandler}>
+                        <FormGroup controlId='email' className='py-2'>
+                            <FormLabel>Email Address</FormLabel>
+                            <FormControl
+                                type='email'
+                                placeholder='Enter email'
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            >
+                            </FormControl>
+                        </FormGroup>
+                        <FormGroup controlId='password' className='py-2'>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl
+                                type='password'
+                                placeholder='Enter password'
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            >
+                            </FormControl>
+                        </FormGroup>
+                        <Button type='submit' variant='primary' className='my-3'>
+                            Sign In
+                        </Button>
+                    </Form>
+                    <Row>
+                        <Col>
+                            New Customer?{' '}
+                            <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>Register</Link>
+                        </Col>
 
-        </Row>
-        
-    </FormContainer>
-  )
+                    </Row>
+                </Col>
+                <Col md={1}></Col>
+                <Col md={5}>
+                    <h5 className='my-1'>Sign in with third party account</h5>
+                        <GoogleButton onClick={googleLoginHandler} className='my-4' />
+                </Col>
+
+            </Row>
+
+        </FormContainer>
+    )
 }
 
 export default LoginScreen
